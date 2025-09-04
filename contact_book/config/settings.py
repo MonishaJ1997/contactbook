15fec03a6a22b56796c0267db7f43a770a09a0a7
MonishaJ1997 <jkmonisha97@gmail.com>
2025-09-04 11:36:51 +0530

    Update settings.py

--- contact_book/config/settings.py
+++ contact_book/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0_b!b8*&6@#e5h+w(=&--7@3@b_xr57b+m+09)^b1bh*o6sr@z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -149,3 +149,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
